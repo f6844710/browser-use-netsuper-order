c4712a1a817e8594881476855dc3db0e19973cb6
f6844710 <f6844710@nifty.com>
2026-01-08 14:02:28 +0900

    main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ NET_SUPER_ID = "00000000000"  # ネットスーパーのイオンID
 NET_SUPER_PASSWORD = "******************"  # ネットスーパーのパスワード
 
 # STT/TTSサーバー設定
-STT_SERVER_URL = "http://100.119.75.44:3000/stt"
+STT_SERVER_URL = "http://192.168.1.5:3000/stt"
 TTS_SERVER_URL = "http://192.168.1.5:10101"
 TTS_SPEAKER_ID = 753902784  # sayo
 
